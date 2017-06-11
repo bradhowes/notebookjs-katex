f686d772a7bc5f420cb6fa3c96ff4bfb8e73348a
Brad Howes <bradhowes@mac.com>
2017-06-11 23:34:40 +0200

    Added assign polyfill

--- index.js
+++ index.js
@@ -14,6 +14,30 @@
 var jpath = require("jsonpath");
 var katex = require("katex");
 
+if (typeof Object.assign != 'function') {
+    Object.assign = function(target, varArgs) { // .length of function is 2
+        if (target == null) { // TypeError if undefined or null
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) { // Skip over if undefined or null
+                for (var nextKey in nextSource) {
+                    // Avoid bugs when hasOwnProperty is shadowed
+                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+        }
+        return to;
+    };
+}
+
 /**
  * Create new KatexFilter object.
  * 
