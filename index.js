0bb436cc79e6108b9b362595141c1a23ae65302c
Brad Howes <bradhowes@mac.com>
2017-06-11 19:08:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+"use strict";
+
+/**
+ * Plugin for Remarkable Markdown processor which transforms $..$ and $$..$$ sequences into math HTML using the
+ * Katex package. To use:
+ * 
+ * var KatexFilter = require('notebookjs-katex');
+ * var kf = new KatexFilter();
+ * ipynb = JSON.parse(fs.readFileSync('path/to/file.ipynb'));
+ * kf.expandKatexInNotebook(ipynb);
+ * notebook = notebookjs.parse(ipynb);
+ */
+
+const jpath = require("jsonpath");
+const katex = require("katex");
+
+/**
+ * Create new KatexFilter object.
+ * 
+ * @param options - options to provide to Katex `renderToString`. Our `renderKatex` method sets `displayMode` and
+ * `throwOnError`.
+ */
+function KatexFilter(katexOptions) {
+    if (!(this instanceof KatexFilter)) return new KatexFilter(katexOptions);
+
+    this.katexOptions = katexOptions || {};
+    this.inlinePattern = /(^|\s)\$([^$]+)\$/g;
+
+    return this;
+}
+
+module.exports = KatexFilter;
+
+KatexFilter.prototype = {
+
+    renderKatex: function(source, displayMode) {
+        var options = Object.assign({}, this.katexOptions);
+        options.displayMode = displayMode;
+        options.throwOnError = false;
+        return katex.renderToString(source, options);
+    },
+
+    /**
+     * Visit each String in the source array and change $$..$$ / $..$ blocks into Katex math HTML.
+     * 
+     * @param source array of String values
+     */
+    expandKatexInTextCellSource: function(source) {
+        var self = this;
+
+        // Regular expression pattern that *should* only match '$..$' sequences, where the first '$' is either
+        // at the beginning of the line or preceded by a space character. Thus, '\$' should *not* begin an
+        // inline math sequence.
+        //
+        var replacer = function(match, p1, p2) { return p1 + self.renderKatex(p2, false); };
+        var inMath = false;
+        var math = '';
+        var newSource = [];
+
+        // General approach - scan each line first for '$$' sequence and process these. For each run of text that
+        // is *not* within a $$..$$ block, scan for $..$ sequences and process those.
+        //
+        for (var index = 0; index < source.length; ++index) {
+            var line = source[index];
+            while (line.length > 0) {
+                var pos = line.indexOf('$$');
+                
+                if (pos == -1) {
+                    if (inMath) {
+                        math += line;
+                    }
+                    else {
+                        newSource.push(line.replace(self.inlinePattern, replacer));
+                    }
+                    break;
+                }
+                
+                if (inMath) {
+                    math += line.substring(0, pos);
+                    newSource.push(self.renderKatex(math, true));
+                    line = line.substring(pos + 2);
+                }
+                else {
+                    
+                    // Found beginning of Katex block
+                    //
+                    math = '';
+                    
+                    if (pos > 0) {
+                        newSource.push(line.substring(0, pos).replace(self.inlinePattern, replacer) + '\n');
+                    }
+                    
+                    line = line.substring(pos + 2);
+                }
+                inMath = !inMath;
+            }
+        }
+        
+        source.length = newSource.length;
+        for (var i = 0; i < newSource.length; ++i) {
+            source[i] = newSource[i];
+        }
+    },
+
+    /**
+     * Visit all 'source' leaf nodes in IPython notebook and replace $$..$$ and $..$ blocks with Katex math
+     * notation.
+     */
+    expandKatexInNotebook: function(ipynb) {
+
+        // Only filter Markdown cells. Just to be safe.
+        //
+        var sources = jpath.query(ipynb, 'cells[?(@.cell_type == "markdown")].source');
+        for (var index = 0; index < sources.length; ++index) {
+            this.expandKatexInTextCellSource(sources[index]);
+        }
+    }
+};
---
+++ index.test.js
@@ -0,0 +1,155 @@
+"use strict";
+
+const vows = require('vows');
+const assert = require('assert');
+const KatexFilter = require('./index.js');
+var kf = new KatexFilter();
+
+vows.describe('KatexFilter').addBatch({
+    'Check regexp matching within line': {
+        topic: function() {
+            const replacer = function(match, p1, p2) { return p1 + 'blah'; };
+            return 'This is $a test$!'.replace(kf.inlinePattern, replacer);
+        },
+        'Expected transform': function(topic) {
+            assert.equal(topic, 'This is blah!');
+        }
+    },
+    'Check regexp matching at start of line': {
+        topic: function() {
+            const replacer = function(match, p1, p2) { return p1 + 'blah'; };
+            return '$This is$ a test!'.replace(kf.inlinePattern, replacer);
+        },
+        'Expected transform': function(topic) {
+            assert.equal(topic, 'blah a test!');
+        }
+    },
+    'Check escaped pattern at start of line': {
+        topic: function() {
+            const replacer = function(match, p1, p2) {
+                return p1 + 'blah';
+            };
+            return '\\$This is$ a test!'.replace(kf.inlinePattern, replacer);
+        },
+        'Expected transform': function(topic) {
+            assert.equal(topic, '\\$This is$ a test!');
+        }
+    },
+    'Check renderKatex': {
+        topic: kf.renderKatex('1 + 2', false),
+        'Should start with <': function(topic) {
+            assert.isTrue(topic.startsWith('<'));
+        },
+        'Should end with >': function(topic) {
+            assert.isTrue(topic.endsWith('>'));
+        },
+        'Should contain 1 + 2': function(topic) {
+            assert.notEqual(topic.indexOf('1 + 2'), -1);
+        }
+    },
+
+    'Convert inline math': {
+        topic: function() {
+            const text = 'What is $1 + 2$?';
+            var source = [text];
+            kf.expandKatexInTextCellSource(source);
+            source.push(text);
+            return source;
+        },
+        'Should start with "What is "': function(topic) {
+            assert.isTrue(topic[0].startsWith('What is '));
+        },
+        'Should end with "?"': function(topic) {
+            assert.isTrue(topic[0].endsWith('?'));
+        },
+        'Should differ from original text': function(topic) {
+            assert.notEqual(topic[0], topic[1]);
+        },
+        'Should contain katex span': function(topic) {
+            assert.notEqual(topic[0].indexOf('<span class="katex">'), -1);
+        },
+        'Should contain <math>': function(topic) {
+            assert.notEqual(topic[0].indexOf('<math>'), -1);
+        },
+        'Should contain math expression': function(topic) {
+
+            // This is brittle since it relies on what current Katex rendering style. Better ideas?
+            //
+            const expected = 'What is <span class="katex"><span class="katex-mathml"><math><semantics><mrow><mn>1</mn><mo>+</mo><mn>2</mn></mrow><annotation encoding="application/x-tex">1 + 2</annotation></semantics></math></span><span class="katex-html" aria-hidden="true"><span class="strut" style="height:0.64444em;"></span><span class="strut bottom" style="height:0.72777em;vertical-align:-0.08333em;"></span><span class="base textstyle uncramped"><span class="mord mathrm">1</span><span class="mbin">+</span><span class="mord mathrm">2</span></span></span></span>?';
+            assert.equal(topic[0], expected);
+        }
+    },
+
+    'Ignore escaped $': {
+        topic: function() {
+            const text = 'The donut costs \$1.50.';
+            var source = [text];
+            kf.expandKatexInTextCellSource(source);
+            source.push(text);
+            return source;
+        },
+        'No change in source': function(topic) {
+            assert.equal(topic[0], topic[1]);
+        }
+    },
+
+    'Ignore lack of terminating $': {
+        topic: function() {
+            const text = 'The donut costs $1.50.';
+            var source = [text];
+            kf.expandKatexInTextCellSource(source);
+            source.push(text);
+            return source;
+        },
+        'No change in source': function(topic) {
+            assert.equal(topic[0], topic[1]);
+        }
+    },
+
+    '$$ block splits lines': {
+        topic: function() {
+            const text = 'The equation $$x + y$$ is very important.';
+            var source = [text];
+            kf.expandKatexInTextCellSource(source);
+            source.push(text);
+            return source;
+        },
+        'Starts with plain text': function(topic) {
+            assert.equal(topic[0], 'The equation \n');
+        },
+        'Then has math span': function(topic) {
+            assert.isTrue(topic[1].startsWith('<span class="katex-display">'));
+        },
+        'Then ends with plain text': function(topic) {
+            assert.equal(topic[2], ' is very important.');
+        }
+    },
+    
+    'Both $$ and $ spans handled properly': {
+        topic: function() {
+            const text = 'The equation $$x + y$$ is only valid when $x$ and $y$ are defined.';
+            var source = [text];
+            kf.expandKatexInTextCellSource(source);
+            source.push(text);
+            return source;
+        },
+        'Contains 3 lines': function(topic) {
+            assert.equal(topic.length - 1, 3);
+        },
+        'Starts with plain text': function(topic) {
+            assert.equal(topic[0], 'The equation \n');
+        },
+        'Then has math span': function(topic) {
+            assert.isTrue(topic[1].startsWith('<span class="katex-display">'));
+        },
+        'Then starts  with plain text': function(topic) {
+            assert.isTrue(topic[2].startsWith(' is only valid when '));
+        },
+        'Then ends with plain text': function(topic) {
+            assert.isTrue(topic[2].endsWith(' are defined.'));
+        },
+        'And has math span': function(topic) {
+            assert.notEqual(topic[2].indexOf('<span class="katex">'), -1);
+        }
+    }
+}).export(module);
